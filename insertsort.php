d437eb82c3839f2458f6d72c8687655f51825b42
Vadim Kutuyev <Vadim Kutuyev>
2016-04-06 00:00:32 -0700

    finished insertion sort

---
+++ insertsort.php
@@ -0,0 +1,38 @@
+<?php
+
+$test = array();
+
+for($i = 0; $i < 10000; $i++){
+	$test[$i] = rand(1,10000);
+}
+
+function insertSort($arr){
+	$time_start = microtime(true);
+
+	for ($i = 1; $i < count($arr); $i++){
+	
+		$count = $i;
+
+		while( $count > 0 && $arr[$count] < $arr[$count - 1] ){
+			$temp = $arr[$count - 1];
+			$arr[$count - 1] = $arr[$count];
+			$arr[$count] = $temp;
+			$count--;
+		}
+
+	}
+	
+	$time_stop = microtime(true);
+	$time = $time_stop - $time_start;
+	echo "Sorted in $time seconds.";
+	
+	return $arr;
+}
+
+
+$endarr = insertSort($test);
+
+var_dump($endarr);
+
+
+?>
